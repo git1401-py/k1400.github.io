dca530ddbefd037b0eeccd1cbd1811c8acf9eac6
kazhal1400 <quick.k1399@gmail.com>
2021-06-30 19:36:46 +0430

    dist commit

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,11 @@ const routes = [
     component: Home,
   },
   {
+    path: "/home",
+    name: "Home",
+    component: Home,
+  },
+  {
     path: "/animation",
     name: "Animation",
     component: Animation,
